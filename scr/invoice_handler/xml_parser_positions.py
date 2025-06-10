8bfe0c9a95f4cb0f88a25fb3f69e0d0275fab969
ECostin84 <82146842+ECostin84@users.noreply.github.com>
2025-06-10 16:51:02 +0200

    Update xml_parser_positions.py

--- scr/invoice_handler/xml_parser_positions.py
+++ scr/invoice_handler/xml_parser_positions.py
@@ -3,6 +3,7 @@ from ..data_class import XmlInvoiceHeader
 from ..data_class import XmlInvoicePosition
 from ..helper_functions import get_xml_tree, find_data_within_element
 from xml.etree.ElementTree import Element
+from ..helper_functions.einvoice_helper import string_to_float
 
 
 # from logger.einvoice_logging import EinvoiceLoger
@@ -10,8 +11,10 @@ from xml.etree.ElementTree import Element
 
 # TODO check with another kreditor (without IBAN)
 # extract xml data from pdf file
-def get_xml_positions(m_cn_id: str, xml_text: str, xml_invoice_data: XmlInvoiceHeader) -> XmlInvoiceHeader:
+def get_xml_positions(m_cn_id: str, xml_text: str, xml_invoice_data: XmlInvoiceHeader, logger) -> XmlInvoiceHeader:
     print("##### START get_zugpferd_positions")
+    logger.info_log(f"START get_xml_header with m_cn_id = {m_cn_id}")
+
     xml_tree: Element = get_xml_tree(xml_text)
     xml_positions_data: Element = xml_tree.find("./SupplyChainTradeTransaction")
     tags_to_search_description: list = ['SpecifiedTradeProduct/Description', 'SpecifiedTradeProduct/Name']
@@ -28,12 +31,12 @@ def get_xml_positions(m_cn_id: str, xml_text: str, xml_invoice_data: XmlInvoiceH
         description_text: str = find_data_within_element(position, tags_to_search_description)[
                                 0:499] if find_data_within_element(position,
                                                                    tags_to_search_description) else "Default text"
-        tax_rate: str = find_data_within_element(position, tags_to_search_tax_rate)
-        quantity: str = find_data_within_element(position, tags_to_search_quantity) if find_data_within_element(
+        tax_rate: float = string_to_float(find_data_within_element(position, tags_to_search_tax_rate))
+        quantity: float = string_to_float(find_data_within_element(position, tags_to_search_quantity)) if find_data_within_element(
             position,
             tags_to_search_quantity) else 1
-        single_net_price: str = find_data_within_element(position, tags_to_search_single_net_price)
-        total_net_price: str = find_data_within_element(position, tags_to_search_total_net_price)
+        single_net_price: float = string_to_float(find_data_within_element(position, tags_to_search_single_net_price))
+        total_net_price: float = string_to_float(find_data_within_element(position, tags_to_search_total_net_price))
 
         article_number: str = ""
         try:
@@ -41,7 +44,7 @@ def get_xml_positions(m_cn_id: str, xml_text: str, xml_invoice_data: XmlInvoiceH
                 article_number: str = re.findall("OE\s*\w{9,}", description_text)[0].replace("OE", "").replace(" ", "")
         except Exception as e:
             print(f"Mistake with article number {e}")
-            # logger.error_log(f"Mistake with positions {e}")
+            logger.error_log(f"Mistake with article number {e}")
 
         xml_invoice_data.add_position(
             XmlInvoicePosition(item_pos=item_position, position_text=description_text, quantity=quantity,
@@ -50,6 +53,6 @@ def get_xml_positions(m_cn_id: str, xml_text: str, xml_invoice_data: XmlInvoiceH
                                article_number=article_number))
         item_position += 1
 
-    # logger.info_log(f"end script zugpferd_extraction")
+    logger.info_log(f"Finish get_xml_header with m_cn_id = {m_cn_id}")
 
     return xml_invoice_data
