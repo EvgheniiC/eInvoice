71c81c6b757cc3a794e926903abc72e33b3ed141
ECostin84 <82146842+ECostin84@users.noreply.github.com>
2025-06-19 09:00:43 +0200

    Update __init__.py

--- scr/helper_functions/__init__.py
+++ scr/helper_functions/__init__.py
@@ -1,1 +1,2 @@
-from .einvoice_helper import find_data_within_element, delete_all_prefills, find_data_with_regex, get_xml_tree, find_data_within_element_with_len, read_xml_file_to_str
+from .einvoice_helper import find_data_within_element, delete_all_prefills, find_data_with_regex, get_xml_tree, \
+    find_data_within_element_with_len, read_xml_file_to_str, get_tags_from_json
