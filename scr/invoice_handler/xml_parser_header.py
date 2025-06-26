29c85b0660bd5368548ac0032692672d53fc8131
ECostin84 <82146842+ECostin84@users.noreply.github.com>
2025-06-26 13:38:26 +0200

    Update xml_parser_header.py

--- scr/invoice_handler/xml_parser_header.py
+++ scr/invoice_handler/xml_parser_header.py
@@ -6,6 +6,7 @@ from ..helper_functions import find_data_within_element, find_data_with_regex, g
 from xml.etree.ElementTree import Element
 
 
+# TODO cam be difference datum formats? %Y%m%d %Y-%m-%d
 # extract xml data from pdf file
 # def zugpferd_extraction(m_cn_id: str, xml_text: str, db_helper, barcode: str):
 def get_xml_header(xml_text: str, xml_invoice_data: XmlInvoiceHeader, logger) -> XmlInvoiceHeader:
@@ -18,11 +19,20 @@ def get_xml_header(xml_text: str, xml_invoice_data: XmlInvoiceHeader, logger) ->
     xml_tree: Element = get_xml_tree(xml_text)
 
     xml_exchanged_document: Element = xml_tree.find("./ExchangedDocument")
+
     xml_invoice_head: Element = xml_tree.find("./SupplyChainTradeTransaction/ApplicableHeaderTradeSettlement")
     xml_invoice_head_money: Element = xml_tree.find(
         "./SupplyChainTradeTransaction/ApplicableHeaderTradeSettlement/SpecifiedTradeSettlementHeaderMonetarySummation")
     xml_supplier_data: Element = xml_tree.find("./SupplyChainTradeTransaction")
     xml_positions_data: Element = xml_tree.find("./SupplyChainTradeTransaction")
+
+    # not zugpferd xml use another tags
+    if not xml_invoice_head:
+        xml_invoice_head: Element = xml_tree
+        xml_positions_data: Element = xml_tree
+        xml_supplier_data: Element = xml_tree
+        xml_invoice_head_money: Element = xml_tree
+        xml_exchanged_document: Element = xml_tree
 
     # header data
     tags_to_search_invoice_number: list = get_tags_from_json('tags_to_search_invoice_number')
@@ -39,62 +49,79 @@ def get_xml_header(xml_text: str, xml_invoice_data: XmlInvoiceHeader, logger) ->
     tags_to_search_tax_amount1: list = get_tags_from_json('tags_to_search_tax_amount1')
     tags_to_search_tax_rate1: list = get_tags_from_json('tags_to_search_tax_rate1')
     tags_to_search_kind_of_invoice: list = get_tags_from_json('tags_to_search_kind_of_invoice')
-    xml_invoice_data.invoice_number = find_data_within_element(xml_exchanged_document,
-                                                               tags_to_search_invoice_number)
+
+    xml_invoice_data.invoice_number = find_data_within_element(xml_exchanged_document, tags_to_search_invoice_number)
 
     try:
         xml_invoice_data.invoice_date = datetime.strptime(
             find_data_within_element(xml_exchanged_document, tags_to_search_invoice_date), '%Y%m%d')
-    except Exception as e:
-        print(f"Invoice date Date was not found {e}")
-        logger.error_log(f"Invoice date bis was not found {e}")
+    except Exception:
+        try:
+            xml_invoice_data.invoice_date = datetime.strptime(
+                find_data_within_element(xml_exchanged_document, tags_to_search_invoice_date), '%Y-%m-%d')
+        except Exception as e:
+            print(f"Invoice date Date was not found {e}")
+            logger.error_log(f"Invoice date bis was not found {e}")
+
+
     try:
         xml_invoice_data.delivery_date = datetime.strptime(
             find_data_within_element(xml_invoice_head, tags_to_search_delivery_date), '%Y%m%d')
     except Exception as e:
-        print(f"Delivery date was not found {e}")
-        logger.error_log(f"Delivery date was not found {e}")
+        try:
+            xml_invoice_data.delivery_date = datetime.strptime(
+                find_data_within_element(xml_invoice_head, tags_to_search_delivery_date), '%Y-%m-%d')
+        except Exception as e:
+            print(f"Delivery date was not found {e}")
+            logger.error_log(f"Delivery date was not found {e}")
 
     try:
         xml_invoice_data.delivery_date_till = datetime.strptime(
             find_data_within_element(xml_invoice_head, tags_to_search_delivery_date_till), '%Y%m%d')
-    except Exception as e:
-        print(f"Delivery date bis was not found {e}")
-        logger.error_log(f"Delivery date bis was not found {e}")
+    except Exception:
+        try:
+            xml_invoice_data.delivery_date_till = datetime.strptime(
+                find_data_within_element(xml_invoice_head, tags_to_search_delivery_date_till), '%Y-%m-%d')
+        except Exception as e:
+            print(f"Delivery date bis was not found {e}")
+            logger.error_log(f"Delivery date bis was not found {e}")
 
     xml_invoice_data.currency = find_data_within_element(xml_invoice_head,
                                                          tags_to_search_currency) if not find_data_within_element(
         xml_invoice_head, tags_to_search_currency) else "EUR"
 
-    xml_invoice_data.order_id = find_data_with_regex(xml_supplier_data, "930\d{7}|960\d{7}")
+    xml_invoice_data.order_id = find_data_with_regex(xml_supplier_data, "930\d{7}|960\d{7}|SIXT-\d{7,}")
     xml_invoice_data.contract_id = find_data_with_regex(xml_supplier_data, r"\bSX-\d{5}(?:-\d{3})?\b")
 
     # SWFM-5293
     if not xml_invoice_data.order_id:
         # sometimes we get order in positions
         logger.info_log(f"Order id was not founded, will find in positions")
-        xml_invoice_data.order_id = find_data_with_regex(xml_positions_data, "930\d{7}|960\d{7}")
+        xml_invoice_data.order_id = find_data_with_regex(xml_positions_data, "930\d{7}|960\d{7}|SIXT-\d{7,}")
 
     if not xml_invoice_data.order_id:
         text_order_id = find_data_within_element(xml_exchanged_document, tags_to_search_order_id)
         if text_order_id:
             try:
-                xml_invoice_data.order_id = re.findall("930\d{7}|960\d{7}", text_order_id)[0]
+                xml_invoice_data.order_id = re.findall("930\d{7}|960\d{7}|SIXT-\d{7,}", text_order_id)[0]
             except Exception as e:
                 print(f"Order was not found {e}")
                 logger.error_log(f"Order bis was not found {e}")
 
-    xml_invoice_data.invoice_amount = find_data_within_element(xml_invoice_head_money, tags_to_search_invoice_amount)
+    xml_invoice_data.invoice_amount = find_data_within_element(xml_invoice_head_money,
+                                                               tags_to_search_invoice_amount)
     xml_invoice_data.total_amount = find_data_within_element(xml_invoice_head_money, tags_to_search_total_amount)
     xml_invoice_data.total_tax_amount = find_data_within_element(xml_invoice_head_money,
                                                                  tags_to_search_total_tax_amount)
     xml_invoice_data.tax_amount1 = find_data_within_element(xml_tree, tags_to_search_tax_amount1)
+    #TODO if w have many tax_amount and tax_rate
     xml_invoice_data.tax_rate1 = find_data_within_element(xml_tree, tags_to_search_tax_rate1)
     xml_invoice_data.supplier = find_data_within_element(xml_supplier_data, tags_to_search_supplier)
     xml_invoice_data.client = "1"
     xml_invoice_data.image_path = xml_invoice_data.barcode + ".pdf"
-    xml_invoice_data.iban = find_data_within_element_with_len(xml_supplier_data, tags_to_search_iban, 22).replace(" ",
-                                                                                                                  "") if find_data_within_element_with_len(
+    xml_invoice_data.iban = find_data_within_element_with_len(xml_supplier_data, tags_to_search_iban, 22).replace(
+        " ",
+        "") if find_data_within_element_with_len(
         xml_supplier_data, tags_to_search_iban, 22) else None
     if xml_invoice_data.iban:
         logger.info_log(f"IBAN was founded = {xml_invoice_data.iban}")
