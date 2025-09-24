11fa8b31f92ff8572f2f6da88dc0f9ec4ffd33f0
ECostin84 <82146842+ECostin84@users.noreply.github.com>
2025-09-24 11:54:47 +0200

    many values were added
    S_KR_STRASSE_DELIVERY, S_KR_ORT_DELIVERY, S_KR_POSTLEITZAHL_DELIVERY, S_KR_CLIENT_NAME_DELIVERY

--- scr/invoice_handler/xml_vendor_parser.py
+++ scr/invoice_handler/xml_vendor_parser.py
@@ -16,10 +16,14 @@ def get_einvoice_vendor_data(m_cn_id: str, xml_text: str, logger) -> (dict, str)
 
     tags_to_search_tax_id: list = get_tags_from_json('tags_to_search_tax_id')
     tags_to_search_client_name: list = get_tags_from_json('tags_to_search_client_name')
+    tags_to_search_client_name_delivery: list = get_tags_from_json('tags_to_search_client_name_delivery')
     tags_to_search_vendor_name: list = get_tags_from_json('tags_to_search_vendor_name')
     tags_to_search_address: list = get_tags_from_json('tags_to_search_address')
+    tags_to_search_address_delivery: list = get_tags_from_json('tags_to_search_address_delivery')
     tags_to_search_postcode: list = get_tags_from_json('tags_to_search_postcode')
+    tags_to_search_postcode_delivery: list = get_tags_from_json('tags_to_search_postcode_delivery')
     tags_to_search_city_name: list = get_tags_from_json('tags_to_search_city_name')
+    tags_to_search_city_name_delivery: list = get_tags_from_json('tags_to_search_city_name_delivery')
     tags_to_search_country: list = get_tags_from_json('tags_to_search_country')
     tags_to_search_iban: list = get_tags_from_json('tags_to_search_iban')
     tags_to_search_vendor: list = get_tags_from_json('tags_to_search_vendor')
@@ -28,11 +32,15 @@ def get_einvoice_vendor_data(m_cn_id: str, xml_text: str, logger) -> (dict, str)
         "M_CN_ID": m_cn_id,
         "S_KR_NAME1": find_data_within_element(xml_vendor_data, tags_to_search_vendor_name),
         "S_KR_STRASSE": find_data_within_element(xml_vendor_data, tags_to_search_address),
+        "S_KR_STRASSE_DELIVERY": find_data_within_element(xml_vendor_data, tags_to_search_address_delivery),
         "S_KR_ORT": find_data_within_element(xml_vendor_data, tags_to_search_city_name),
+        "S_KR_ORT_DELIVERY": find_data_within_element(xml_vendor_data, tags_to_search_city_name_delivery),
         "S_KR_POSTLEITZAHL": find_data_within_element(xml_vendor_data, tags_to_search_postcode),
+        "S_KR_POSTLEITZAHL_DELIVERY": find_data_within_element(xml_vendor_data, tags_to_search_postcode_delivery),
         "S_KR_LAND": find_data_within_element(xml_vendor_data, tags_to_search_country),
         "S_KR_USTID": find_data_within_element(xml_vendor_data, tags_to_search_tax_id),
         "S_KR_CLIENT_NAME": find_data_within_element(xml_vendor_data, tags_to_search_client_name),
+        "S_KR_CLIENT_NAME_DELIVERY": find_data_within_element(xml_vendor_data, tags_to_search_client_name_delivery),
         "S_KR_IBAN": find_data_within_element_with_len(xml_vendor_data, tags_to_search_iban, 22).replace(" ",
                                                                                                          "") if find_data_within_element_with_len(
             xml_vendor_data, tags_to_search_iban, 22) else None
--- tests/test_xml_vendor_parser.py
+++ tests/test_xml_vendor_parser.py
@@ -4,16 +4,24 @@ from unittest.mock import Mock
 from scr.helper_functions.einvoice_helper import read_xml_file_to_str
 
 
-# TODO can i find supplier?
 class TestClientParser(unittest.TestCase):
     def test_get_einvoice_client_data(self):
         m_cn_id = "5208214"
         xml_text_from_xml = read_xml_file_to_str('xml_files/xml_text_from_xml.xml')
         clients_data, supplier = get_einvoice_vendor_data(m_cn_id=m_cn_id, xml_text=xml_text_from_xml, logger=Mock())
-        self.assertEqual(clients_data, {'M_CN_ID': '5208214', 'S_KR_NAME1': '[Seller name]',
-                                        'S_KR_STRASSE': '[Seller address line 1]', 'S_KR_ORT': '[Seller city]',
-                                        'S_KR_POSTLEITZAHL': '12345', 'S_KR_LAND': 'DE', 'S_KR_USTID': 'DE 123456789',
-                                        'S_KR_IBAN': 'DE75512108001245126199','S_KR_CLIENT_NAME': '[Buyer name]'})
+        self.assertEqual(clients_data, {'M_CN_ID': '5208214',
+                                        'S_KR_CLIENT_NAME': '[Buyer name]',
+                                        'S_KR_CLIENT_NAME_DELIVERY': None,
+                                        'S_KR_IBAN': 'DE75512108001245126199',
+                                        'S_KR_LAND': 'DE',
+                                        'S_KR_NAME1': '[Seller name]',
+                                        'S_KR_ORT': '[Seller city]',
+                                        'S_KR_ORT_DELIVERY': None,
+                                        'S_KR_POSTLEITZAHL': '12345',
+                                        'S_KR_POSTLEITZAHL_DELIVERY': None,
+                                        'S_KR_STRASSE': '[Seller address line 1]',
+                                        'S_KR_STRASSE_DELIVERY': None,
+                                        'S_KR_USTID': 'DE 123456789'})
         self.assertEqual(supplier, None)
 
     def test_get_einvoice_client_data_none(self):
@@ -23,12 +31,16 @@ class TestClientParser(unittest.TestCase):
         self.assertEqual(clients_data,
                          {'M_CN_ID': '5208215',
                           'S_KR_CLIENT_NAME': 'Abweichender Handelsname Rechnungsempfnger',
+                          'S_KR_CLIENT_NAME_DELIVERY': 'Anderer Leistungsempfnger',
                           'S_KR_IBAN': None,
                           'S_KR_LAND': 'DK',
                           'S_KR_NAME1': 'EntServ Deutschland GmbH',
                           'S_KR_ORT': 'Ort Rechnungssteller',
+                          'S_KR_ORT_DELIVERY': 'Anderer Leistungsempfnger Ort',
                           'S_KR_POSTLEITZAHL': '12345',
+                          'S_KR_POSTLEITZAHL_DELIVERY': '45678',
                           'S_KR_STRASSE': 'Strae Rechnungssteller 1',
+                          'S_KR_STRASSE_DELIVERY': 'Anderer Leistungsempfnger Strae 1',
                           'S_KR_USTID': 'ATU13585627'}
                          )
         self.assertEqual(supplier, None)
@@ -39,9 +51,19 @@ class TestClientParser(unittest.TestCase):
         clients_data, supplier = get_einvoice_vendor_data(m_cn_id=m_cn_id, xml_text=xml_text_from_zugpferd,
                                                           logger=Mock())
         self.assertEqual(clients_data,
-                         {'M_CN_ID': '5207492', 'S_KR_NAME1': 'KMLZ Rechtsanwaltsges. mbH',
-                          'S_KR_STRASSE': 'Unterer Anger 3', 'S_KR_ORT': 'Munchen', 'S_KR_POSTLEITZAHL': '80331',
-                          'S_KR_LAND': 'DE', 'S_KR_USTID': 'DE814742004', 'S_KR_IBAN': 'DE95700400410228840500', 'S_KR_CLIENT_NAME': 'Sixt SE',}
+                         {'M_CN_ID': '5207492',
+                          'S_KR_CLIENT_NAME': 'Sixt SE',
+                          'S_KR_CLIENT_NAME_DELIVERY': None,
+                          'S_KR_IBAN': 'DE95700400410228840500',
+                          'S_KR_LAND': 'DE',
+                          'S_KR_NAME1': 'KMLZ Rechtsanwaltsges. mbH',
+                          'S_KR_ORT': 'Munchen',
+                          'S_KR_ORT_DELIVERY': None,
+                          'S_KR_POSTLEITZAHL': '80331',
+                          'S_KR_POSTLEITZAHL_DELIVERY': None,
+                          'S_KR_STRASSE': 'Unterer Anger 3',
+                          'S_KR_STRASSE_DELIVERY': None,
+                          'S_KR_USTID': 'DE814742004'}
                          )
         self.assertEqual(supplier, None)
         # TODO potential supplier
@@ -52,9 +74,19 @@ class TestClientParser(unittest.TestCase):
         xml_test_iban_none = read_xml_file_to_str('xml_files/xml_test_iban_none.xml')
         clients_data, supplier = get_einvoice_vendor_data(m_cn_id=m_cn_id, xml_text=xml_test_iban_none, logger=Mock())
         self.assertEqual(clients_data,
-                         {'M_CN_ID': '5207492', 'S_KR_NAME1': 'E.ON Energie Deutschland GmbH',
-                          'S_KR_STRASSE': 'Postfach 14 75', 'S_KR_ORT': 'Landshut', 'S_KR_POSTLEITZAHL': '84001',
-                          'S_KR_LAND': 'DE', 'S_KR_USTID': 'DE259922663', 'S_KR_IBAN': None, 'S_KR_CLIENT_NAME': 'Sixt GmbH & Co. Autovermietung KG',}
+                         {'M_CN_ID': '5207492',
+                          'S_KR_CLIENT_NAME': 'Sixt GmbH & Co. Autovermietung KG',
+                          'S_KR_CLIENT_NAME_DELIVERY': None,
+                          'S_KR_IBAN': None,
+                          'S_KR_LAND': 'DE',
+                          'S_KR_NAME1': 'E.ON Energie Deutschland GmbH',
+                          'S_KR_ORT': 'Landshut',
+                          'S_KR_ORT_DELIVERY': None,
+                          'S_KR_POSTLEITZAHL': '84001',
+                          'S_KR_POSTLEITZAHL_DELIVERY': None,
+                          'S_KR_STRASSE': 'Postfach 14 75',
+                          'S_KR_STRASSE_DELIVERY': None,
+                          'S_KR_USTID': 'DE259922663'}
                          )
         self.assertEqual(supplier, None)
 
@@ -65,14 +97,18 @@ class TestClientParser(unittest.TestCase):
         clients_data, supplier = get_einvoice_vendor_data(m_cn_id=m_cn_id, xml_text=clear_xml_from_gerd, logger=Mock())
         self.assertEqual(clients_data,
                          {'M_CN_ID': '5207492',
+                          'S_KR_CLIENT_NAME': None,
+                          'S_KR_CLIENT_NAME_DELIVERY': None,
                           'S_KR_IBAN': 'DE13664900000023969700',
                           'S_KR_LAND': 'DE',
                           'S_KR_NAME1': 'Inspire Technologies GmbH',
                           'S_KR_ORT': 'St. Georgen',
+                          'S_KR_ORT_DELIVERY': None,
                           'S_KR_POSTLEITZAHL': '78112',
+                          'S_KR_POSTLEITZAHL_DELIVERY': None,
                           'S_KR_STRASSE': 'Leopoldstr. 1',
-                          'S_KR_USTID': 'DE260569738',
-                          'S_KR_CLIENT_NAME': None,})
+                          'S_KR_STRASSE_DELIVERY': None,
+                          'S_KR_USTID': 'DE260569738'})
 
         self.assertEqual(supplier, None)
 
@@ -82,17 +118,20 @@ class TestClientParser(unittest.TestCase):
         clients_data, supplier = get_einvoice_vendor_data(m_cn_id=m_cn_id, xml_text=clear_xml_from_gerd, logger=Mock())
         self.assertEqual(clients_data,
                          {'M_CN_ID': '5207492',
+                          'S_KR_CLIENT_NAME': 'Sixt SE',
+                          'S_KR_CLIENT_NAME_DELIVERY': None,
                           'S_KR_IBAN': 'DE95700400410228840500',
                           'S_KR_LAND': 'DE',
                           'S_KR_NAME1': 'KMLZ Rechtsanwaltsges. mbH',
                           'S_KR_ORT': 'Munchen',
+                          'S_KR_ORT_DELIVERY': None,
                           'S_KR_POSTLEITZAHL': '80331',
+                          'S_KR_POSTLEITZAHL_DELIVERY': None,
                           'S_KR_STRASSE': 'Unterer Anger 3',
-                          'S_KR_USTID': 'DE814742004',
-                          'S_KR_CLIENT_NAME': 'Sixt SE'})
+                          'S_KR_STRASSE_DELIVERY': None,
+                          'S_KR_USTID': 'DE814742004'})
 
         self.assertEqual(supplier, None)
-
 
     def test_get_einvoice_SWFM_5490(self):
         m_cn_id = "5207492"
@@ -101,15 +140,20 @@ class TestClientParser(unittest.TestCase):
         self.assertEqual(clients_data,
                          {'M_CN_ID': '5207492',
                           'S_KR_CLIENT_NAME': 'Sixt GmbH & Co Autovermietg.KG Reparaturzentrum Berlin',
+                          'S_KR_CLIENT_NAME_DELIVERY': 'Sixt GmbH & Co Autovermietg.KG Reparaturzentrum Berlin',
                           'S_KR_IBAN': 'DE55160500003504000405',
                           'S_KR_LAND': 'DE',
                           'S_KR_NAME1': 'Autohaus Babelsberg  GmbH & Co.KG',
                           'S_KR_ORT': 'Potsdam',
+                          'S_KR_ORT_DELIVERY': 'Schnefeld',
                           'S_KR_POSTLEITZAHL': '14482',
+                          'S_KR_POSTLEITZAHL_DELIVERY': '12529',
                           'S_KR_STRASSE': 'Fritz-Zubeil-Strae 70-78',
+                          'S_KR_STRASSE_DELIVERY': 'Am Airport 7',
                           'S_KR_USTID': 'DE138410797'})
 
         self.assertEqual(supplier, None)
+
 
 if __name__ == '__main__':
     unittest.main()
